e2224dd7a2d45ded16620d944500c7a0bbb89eb7
israelfi <israelfilipe.sa@gmail.com>
2021-01-17 13:57:14 -0300

    Minor changes

--- scripts/tbug_V2.py
+++ scripts/tbug_V2.py
@@ -18,8 +18,8 @@ class bug:
 		self.curve_pos = [px, py]
 
 		self.lidar_raw = []
-		self.lidar_x = []
-		self.lidar_y = []
+		self.lidar_x = [0] * 360
+		self.lidar_y = [0] * 360
 		self.l_max = 0
 		self.l_min = 0
 		self.vel_msg = Twist()
@@ -51,10 +51,10 @@ class bug:
 
 		alfa = 0
 		for x in self.lidar_raw:
-			sx = (cos(self.robot_ori)*(x*cos(np.deg2rad(alfa))) + sin(self.robot_ori)*(x*sin(np.deg2rad(alfa)))) +self.robot_pos[0]
-			sy = (-sin(self.robot_ori)*(x*cos(np.deg2rad(alfa))) + cos(self.robot_ori)*(x*sin(np.deg2rad(alfa)))) +self.robot_pos[1]
-			self.lidar_x.append(sx)
-			self.lidar_y.append(sy)
+			sx = (cos(self.robot_ori)*(x*cos(np.deg2rad(alfa-180))) - sin(self.robot_ori)*(x*sin(np.deg2rad(alfa-180)))) +self.robot_pos[0]
+			sy = (sin(self.robot_ori)*(x*cos(np.deg2rad(alfa-180))) + cos(self.robot_ori)*(x*sin(np.deg2rad(alfa-180)))) +self.robot_pos[1]
+			self.lidar_x[alfa]= sx
+			self.lidar_y[alfa] = sy
 			alfa = alfa+1
 
 
@@ -63,37 +63,49 @@ class bug:
 		return d
 
 	def find_endpoints(self):
-		a = min(2,self.l_max)
-		ind = 0.1*a
+		# a = min(2,self.l_max)
+		# ind = 0.1*a
 
-		dist = np.array(self.lidar_raw)
+		# dist = np.array(self.lidar_raw)
 
-		for i in range(len(dist)):
-			if(dist[i] >= self.l_max):
-				dist[i] = 0
+		# for i in range(len(dist)):
+		# 	if(dist[i] >= self.l_max):
+		# 		dist[i] = 0
 
-		np.append(dist,dist[0])
-		dist2 = np.append(dist[1:len(dist)], 0)
-		duum = np.abs(dist2 - dist)
-		duum = duum[0:len(duum)-1]
-		idx = []
-		for i in range(len(duum)):
-			if(duum[i] > ind):
-				idx.append(i)
+		# np.append(dist,dist[0])
+		# dist2 = np.append(dist[1:len(dist)], 0)
+		# duum = np.abs(dist2 - dist)
+		# duum = duum[0:len(duum)-1]
+		# idx = []
+		# for i in range(len(duum)):
+		# 	if(duum[i] > ind):
+		# 		idx.append(i)
+		# print idx
+		# return idx
+		end_ids = []
+		cont = 0
+		for i in range(1, len(self.lidar_raw)-1):
+			if(self.l_min < self.lidar_raw[i] < self.l_max):
+				if(self.lidar_raw[i+1] >= self.l_max):
+					end_ids.append(i)
+				elif(self.lidar_raw[i-1] >= self.l_max):
+					end_ids.append(i)
 
-		return idx
+
+		return end_ids
 
 
 	def best_obstacle(self,endp):
 		d_f_n = 1000000
 		for i in range(len(endp)):
-						dummy_f = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]]) - np.array([self.curve_pos[0],self.curve_pos[1]])
-						dummy_f = np.sqrt(np.sum(dummy_f**2))
-						dummy_d = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]]) - np.array([self.robot_pos[0],self.robot_pos[1]])
-						dummy_d = np.sqrt(np.sum(dummy_f**2))
-						if(d_f_n > dummy_f + dummy_d):
-							d_f_n = dummy_f + dummy_d
-							go_end_p = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]])
+			dummy_f = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]]) - np.array([self.curve_pos[0],self.curve_pos[1]])
+			dummy_f = np.sqrt(np.sum(dummy_f**2))
+			dummy_d = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]]) - np.array([self.robot_pos[0],self.robot_pos[1]])
+			dummy_d = np.sqrt(np.sum(dummy_d**2))
+			# print "\nendp:", endp[i], "dummy_f:", dummy_f, "dummy_d:", dummy_d, "\n"
+			if(d_f_n > dummy_f + dummy_d):
+				d_f_n = dummy_f + dummy_d
+				go_end_p = np.array([self.lidar_x[endp[i]], self.lidar_y[endp[i]]])
 
 		return d_f_n, go_end_p
 
@@ -198,11 +210,11 @@ def follow():
 			ori_deg = np.rad2deg(ori_rad)
 			
 			theta_q_idx = theta_q - ori_deg + 180
-			print '\ntheta:', theta_q, '| robot:', ori_deg
+			# print '\ntheta:', theta_q, '| robot:', ori_deg
 			if theta_q_idx > 360:
 				theta_q_idx -= 360
 
-			print 'theta_idx:', theta_q_idx
+			# print 'theta_idx:', theta_q_idx
 
 			# Leituras do laser
 			do = Tbug.lidar_raw
@@ -214,7 +226,7 @@ def follow():
 			
 
 			# Follow Target	
-			print(do[int(floor(theta_q_idx))], Tbug.l_max, dq, do_min, obst_detec)		
+			# print(do[int(floor(theta_q_idx))], Tbug.l_max, dq, do_min, obst_detec)		
 			if( (do[int(floor(theta_q_idx))] >= min([Tbug.l_max, dq])) and (do[int(ceil(theta_q_idx))+1] >= min([Tbug.l_max, dq])) and (do_min > obst_detec)):
 			# if( (do[int(floor(theta_q)+1)] >= min([Tbug.l_max, dq])) and (do[int(ceil(theta_q))+1] >= min([Tbug.l_max, dq])) and (do_min > obst_detec)):
 				print("Going to the Taget\n")
@@ -225,6 +237,7 @@ def follow():
 				if (Tbug.distancy() < delta):
 					print("Target Founded!\n")
 					px, py, = raw_input('Insira o valor do proximo destino em x e y (valores separados por espaco, considere o tamanho do mapa 30x30): ').split()
+					d_f = 1000000
 					px, py = [float(i) for i in [px, py]]
 					Tbug.curve_pos = [px, py]
 
@@ -234,10 +247,15 @@ def follow():
 			else:
 				endp = Tbug.find_endpoints()
 				if(endp):
+					endp = Tbug.find_endpoints()
+					## print(endp)
+					# Dist. e coordenadas do ponto de descontinuidade que minimza a dist ate o alvo
 					d_f_n, go_end_p = Tbug.best_obstacle(endp)
-	
-					if(d_f_n < d_f):
-						print("Going for the best obstacle avoidance")
+					print 'Endpoint:', go_end_p
+
+					print "d_f_n:", d_f_n, "| d_f:", d_f, "\n"	
+					if not (d_f_n > d_f):
+						print("Going for the best obstacle avoidance.")
 						d_f = d_f_n
 						go_end = go_end_p
 						dummy_d2 = go_end - np.array([Tbug.robot_pos[0],Tbug.robot_pos[1]])
@@ -246,8 +264,27 @@ def follow():
 						do_min, idx_do_min = Tbug.min_dist()
 						if(do_min > obst_detec):
 							t = rospy.get_time() - t_init
-							rx, ry = Tbug.find_curve(go_end[0],go_end[1],t)
+							# rx, ry = Tbug.find_curve(go_end[0],go_end[1],t)
+							rx, ry = go_end[0], go_end[1]
+							## print(rx, ry)
 							Tbug.follow_target(rx, ry)
+
+						# while(do_min > obst_detec and not (d_f_n > d_f)):
+						# 	print "\nd_f_n:", d_f_n, "| d_f:", d_f
+						# 	print("Going for the best obstacle avoidance")
+						# 	d_f_n, go_end_p = Tbug.best_obstacle(endp)
+						# 	d_f = d_f_n
+						# 	go_end = go_end_p
+						# 	dummy_d2 = go_end - np.array([Tbug.robot_pos[0],Tbug.robot_pos[1]])
+						# 	dummy_angle = atan2(dummy_d2[1], dummy_d2[0])
+						# 	dummy_d2 = sqrt(np.sum(dummy_d2**2))
+						# 	do_min, idx_do_min = Tbug.min_dist()
+						# 	t = rospy.get_time() - t_init
+						# 	# rx, ry = Tbug.find_curve(go_end[0],go_end[1],t)
+						# 	rx, ry = go_end[0], go_end[1]
+						# 	print(rx, ry)
+						# 	Tbug.follow_target(rx, ry)
+
 					else:
 						d_re = d_f
 						d_angle = dummy_angle
